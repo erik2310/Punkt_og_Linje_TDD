3be6337fca0fc4b46544a39b3a7a121c2e88e542
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-09-30 19:31:11 +0200

    Tilføjet Punkt klassen.

---
+++ src/Punkt.java
@@ -0,0 +1,20 @@
+public class Punkt {
+
+    // Deklarer int x og y datatyper
+    private int x, y;
+
+    // En constructor der sætter x og y
+    public Punkt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters til x og y
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
